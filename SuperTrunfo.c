e2a1dadbb30f8dcd6f9b613b41a63af7d220eaea
Mariane Nojosa <marianenojosa007@gmail.com>
2025-09-16 00:52:39 +0000

    organização do cpódigo

--- SuperTrunfo.c
+++ SuperTrunfo.c
@@ -1,224 +1,99 @@
 #include <stdio.h>
-int main (){
 
-//variáveis da carta 01:
+int main () {
+
+    //variáveis carta 01:
 
     char estado1;
-    char código1 [10];
+    int codigo1;
     char nome_do_estado1 [20];
-    int população1;
-    float área1;
+    int populacao1;
+    float area1;
     float PIB1;
     int pontos_turisticos1;
 
-//cadastro da carta 01:
-
-printf("CADASTRO DA CARTA 01 \n");
-
-printf("Digite a inicial do estado: \n");
-scanf("%s", &estado1);
-
-printf("Digite o código da carta: \n");
-scanf("%s", código1);
-
-printf("Digite o nome do estado: \n");
-scanf("%s", nome_do_estado1);
-
-printf("Qual a população? \n");
-scanf("%d", &população1);
-
-printf("Qual o tamanho em área? \n");
-scanf("%d", &área1);
-
-printf("Qual o PIB? \n");
-scanf("%d", &PIB1);
-
-printf("Qual o número de pontos turísticos? \n");
-scanf("%d", &pontos_turisticos1);
-
-printf("Cadastro da carta 01 realizado! \n");
-
- //variáveis da carta 02:
+    //variáveis carta 02:
 
     char estado2;
-    char código2 [10];
+    int codigo2;
     char nome_do_estado2 [20];
-    int população2;
-    float área2;
+    int populacao2;
+    float area2;
     float PIB2;
     int pontos_turisticos2;
+ 
+    //cadastro da carta 01:
 
-//cadastro da carta 02:
+    printf("----- CADASTRO DA CARTA 01 -----\n");
 
-printf("CADASTRO DA CARTA 02 \n");
+    printf("Digite a inicial do estado: \n");
+    scanf("%c", &estado1);
 
-printf("Digite a inicial do estado: \n");
-scanf("%s", &estado2);
+    printf("Digite o código da carta: \n");
+    scanf("%d", &codigo1);
 
-printf("Digite o código da carta: \n");
-scanf("%s", código2);
+    printf("Digite o nome do estado: \n");
+    scanf(" %s", nome_do_estado1); //Foi adicionado um espaço antes do %s para resolver o seginte bug: https://stackoverflow.com/questions/29775323/scanfc-call-seems-to-be-skipped
 
-printf("Digite o nome do estado: \n");
-scanf("%s", nome_do_estado2);
+    printf("Qual a população? \n");
+    scanf("%d", &populacao1);
 
-printf("Qual a população? \n");
-scanf("%d", &população2);
+    printf("Qual o tamanho em área? \n");
+    scanf("%f", &area1);
 
-printf("Qual o tamanho em área? \n");
-scanf("%d", &área2);
+    printf("Qual o PIB? \n");
+    scanf("%f", &PIB1);
 
-printf("Qual o PIB? \n");
-scanf("%d", &PIB2);
+    printf("Qual o número de pontos turísticos? \n");
+    scanf("%d", &pontos_turisticos1);
 
-printf("Qual o número de pontos turísticos? \n");
-scanf("%d", &pontos_turisticos2);
+    printf("Cadastro da carta 01 realizado! \n\n");
 
-printf("Cadastro da Carta 02 realizado! \n");
 
-// Exibição de cartas cadastradas:
+    //cadastro da carta 02:
+    printf("----- CADASTRO DA CARTA 02 -----\n");
 
-printf("Carta 01 \n");
-printf("Estado: %s \n", estado1);
+    printf("Digite a inicial do estado: \n");
+    scanf(" %c", &estado2); 
+    printf("Digite o código da carta: \n");
+    scanf("%d", &codigo2);
 
-printf("Código da Carta: %d \n", código1);
-printf("Nome do Estado: %s \n", nome_do_estado1);
-printf("População: %d \n", população1);
-printf("Área: %d \n", área1);
-printf("PIB: %d \n", PIB1);
-printf("Pontos turísticos: %d \n", pontos_turisticos1);
+    printf("Digite o nome do estado: \n");
+    scanf("%s", nome_do_estado2);
 
-printf("Carta 02 \n");
-printf("Estado: %s \n", estado2);
-printf("Código da Carta: %d \n", código2);
-printf("Nome do Estado: %s \n", nome_do_estado2);
-printf("População: %d \n", população2);
-printf("Área: %d \n", área2);
-printf("PIB: %d \n", PIB2);
-printf("Pontos turísticos: %d \n", pontos_turisticos2);
+    printf("Qual a população? \n");
+    scanf("%d", &populacao2);
 
-return 0;
+    printf("Qual o tamanho em área? \n");
+    scanf("%f", &area2);
 
+    printf("Qual o PIB? \n");
+    scanf("%f", &PIB2);
 
+    printf("Qual o número de pontos turísticos? \n");
+    scanf("%d", &pontos_turisticos2);
 
+    printf("Cadastro da Carta 02 realizado! \n\n");
 
+    // Exibição de cartas cadastradas:
 
+    printf("----- CARTA 01 -----\n\n");
+    printf("Estado: %c \n", estado1);
+    printf("Código da Carta: %d \n", codigo1);
+    printf("Nome do Estado: %s \n", nome_do_estado1);
+    printf("População: %d \n", populacao1);
+    printf("Área: %f \n", area1);
+    printf("PIB: %f \n", PIB1);
+    printf("Pontos turísticos: %d \n\n", pontos_turisticos1);
 
+    printf("----- CARTA 02 -----\n\n");
+    printf("Estado: %c \n", estado2);
+    printf("Código da Carta: %d \n", codigo2);
+    printf("Nome do Estado: %s \n", nome_do_estado2);
+    printf("População: %d \n", populacao2);
+    printf("Área: %f \n", area2);
+    printf("PIB: %f \n", PIB2);
+    printf("Pontos turísticos: %d \n", pontos_turisticos2);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
